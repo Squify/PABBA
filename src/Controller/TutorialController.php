8e450b23f4b1ac0b23d5ab01ca53d232f8d9fbb0
benjamin <emailpourfb90@gmail.com>
2021-04-21 22:13:04 +0200

    test service account

--- src/Controller/TutorialController.php
+++ src/Controller/TutorialController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 
 use App\Repository\PlaceRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Google\Client;
 use Google_Client;
+use Google_Service_YouTube;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -51,16 +53,25 @@ class TutorialController extends AbstractController
      */
     public function upload()
     {
-        $client = new Google_Client();
-        $client->setClientId('972402334252-o6l8kn2quaqsko00ejmlnaf0403hd1jd.apps.googleusercontent.com');
-        $client->setClientSecret('HTHz_hdrgPxFiatJODd3sX7e');
-        $client->setRedirectUri('http://localhost:33/tutoriel/upload');
-        $client->setScopes(
-            ['https://www.googleapis.com/auth/youtube.upload']
-        );
-        $client->setAccessType('offline');
+        $client = new Client();
+        $client->setAuthConfig(realpath(__DIR__ . '/../../var/credentials.json'));
+        $client->setApplicationName("test upload");
+        $client->setScopes(['https://www.googleapis.com/auth/youtube.readonly']);
+        $youtube = new Google_Service_YouTube($client);
 
-        $youtube = new \Google_Service_YouTube($client);
+        $response = $youtube->search->listSearch('id,snippet', [
+            'q' => 'racoon',
+            'order' => 'relevance',
+            'maxResults' => 10,
+            'type' => 'video'
+        ]);
+
+
+        $first = $youtube->videos->listVideos('id,snippet,contentDetails', [
+            'id' => $response['items'][0]['id']['videoId']
+        ]);
+        dump($response, $first);
+        die;
 
 
         return $this->render("tutorial/upload.html.twig", [
