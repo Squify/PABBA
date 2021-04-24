b56ea1ad5c58c6c7fc6ef4ac1ead01a27d7de0ad
Aurelien <aureleinguillemot@hotmail.fr>
2021-04-24 12:56:05 +0200

    Connection google OK, upload possible

--- src/Controller/TutorialController.php
+++ src/Controller/TutorialController.php
@@ -53,28 +53,58 @@ class TutorialController extends AbstractController
      */
     public function upload()
     {
-        $client = new Client();
+        $connect = 0;
+        $client = new Google_Client();
+/*        $client = new Client();
         $client->setAuthConfig(realpath(__DIR__ . '/../../var/credentials.json'));
-        $client->setApplicationName("test upload");
-        $client->setScopes(['https://www.googleapis.com/auth/youtube.upload']);
+        $client->setApplicationName("test upload");*/
+        $client->setClientId('972402334252-o6l8kn2quaqsko00ejmlnaf0403hd1jd.apps.googleusercontent.com');
+        $client->setClientSecret('HTHz_hdrgPxFiatJODd3sX7e');
+        $client->setRedirectUri('http://localhost:33/tutoriel/upload');
+        $client->setScopes(['https://www.googleapis.com/auth/youtube']);
+
         $youtube = new Google_Service_YouTube($client);
+        if(isset( $_GET['code'])) {
+            $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+            dump($token);
+            $connect = 1;
+        }
 
-        $response = $youtube->videos->insert(
-            'id',
-            new \Google_Service_YouTube_Video(),
-            [
-                'data' => file_get_contents('/Users/benjamin/Pictures/Screenshot/test.mov'),
-                'mimeType' => 'application/octet-stream',
-                'uploadType' => 'multipart'
-            ]
-        );
-        dump($response);
-        die;
+        // gérer le fait de ne pas avoir à se reconnecter à chaque fois qu'on reload la page
+        /*  if(isset($_SESSION['token']))
+        {
+            $client->setAccessToken($_SESSION['token']);
+        }*/
+        dd(__DIR__);
+        if($client->getAccessToken()) {
 
+            $snippet = new \Google_Service_YouTube_VideoSnippet();
+            $snippet->setTitle('Vidéo de test');
+            $snippet->setDescription('Test de description');
+            $snippet->setTags(['tag1', 'tag2', 'tag3']);
+            $snippet->setCategoryId('17');
+
+            $status = new \Google_Service_YouTube_VideoStatus();
+            $status->setPrivacyStatus('private'); // PRIVATE / UNLISTED / PUBLIC
+
+
+            $video = new \Google_Service_YouTube_Video();
+            $video->setSnippet($snippet);
+            $video->setStatus($status);
+
+            $client->setDefer(true);
+            $request = $youtube->videos->insert('status,snippet', $video);
+            $file = dirname(__DIR__).'/video.mp4';
+            $media = new \Google_Http_MediaFileUpload($client, $request, 'video/*', file_get_contents($file));
+
+            $video = $client->execute($request);
+            dump($video);
+        }
 
         return $this->render("tutorial/upload.html.twig", [
             'client' => $client,
             'authUrl' => $client->createAuthUrl(),
+            'connect' => $connect,
         ]);
     }
 
